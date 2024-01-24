7d639923a8c329ab64fc34d91d1a84234916b4f5
langnv <lang.nv1@cmctelecom.vn>
2024-01-24 17:56:15 +0700

    create accessToken, refreshToken

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,7 +1,9 @@
+import { ParamsDictionary } from 'express-serve-static-core'
 import { Request, Response } from 'express'
 import usersService from '~/services/users.services'
+import { RegisterReqBody } from '~/models/request/User.requests'
 
-export const loginController = (req: Request, res: Response) => {
+export const loginController = (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
   const { email, password } = req.body
   if (email === 'langnv' && password === '123') {
     res.status(201).json({ message: 'login success' })
@@ -9,9 +11,8 @@ export const loginController = (req: Request, res: Response) => {
   res.status(400).json({ message: 'login fail' })
 }
 export const registerController = async (req: Request, res: Response) => {
-  const { email, password } = req.body
   try {
-    const result = await usersService.register({ email, password })
+    const result = await usersService.register(req.body)
     res.status(201).json({ message: 'Register success', result })
   } catch (error) {
     res.status(400).json({ message: 'login fail', error })
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,17 +1,45 @@
+import { TokenType } from '~/constants/enums'
+import { RegisterReqBody } from '~/models/request/User.requests'
 import User from '~/models/schemas/User.schemas'
 import databaseService from '~/services/database.services'
+import { hashPassword } from '~/utils/crypto'
+import { signToken } from '~/utils/jwt'
 
 class UsersService {
-  async register(payload: { email: string; password: string }) {
-    const { email, password } = payload
-
+  private signAccessToken(user_id: string) {
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.AccessToken
+      },
+      options: {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN
+      }
+    })
+  }
+  private signRefreshToken(user_id: string) {
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.RefreshToken
+      },
+      options: {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN
+      }
+    })
+  }
+  async register(payload: RegisterReqBody) {
     const result = await databaseService.users.insertOne(
       new User({
-        email,
-        password
+        ...payload,
+        date_of_birth: new Date(payload.date_of_birth),
+        password: hashPassword(payload.password)
       })
     )
-    return result
+    const userId = result.insertedId.toString()
+    const [accessToken, refreshToken] = await Promise.all([this.signAccessToken(userId), this.signRefreshToken(userId)])
+
+    return { accessToken, refreshToken }
   }
   async checkEmailExist(email: string) {
     const user = await databaseService.users.findOne({ email })
